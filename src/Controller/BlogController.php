819c28991db30c32d231ebce820a6fc771705b7a
Dohs <spinery@hotmail.fr>
2019-05-07 13:15:39 +0200

    symfony quest 4

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog/show/{slug}",requirements={"slug"="[a-z0-9-]+"})
+     * @return Response
+     */
+    public function show($slug = 'Article Sans Titre'): Response
+    {
+        return $this->render('blog/show.html.twig',
+            ['show' =>ucwords($slug,'-')]
+        );
+    }
+}
