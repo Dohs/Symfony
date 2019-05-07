7d57013d897f6f3b77c01ad57a8fbe06d3bac701
Dohs <spinery@hotmail.fr>
2019-05-07 13:20:12 +0200

    add str_replace

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,13 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/show/{slug}",requirements={"slug"="[a-z0-9-]+"})
+     * @Route("/blog/show/{slug}",requirements={"slug"="[a-z0-9-]+"},defaults={"slug"="article-sans-titre"})
      * @return Response
      */
-    public function show($slug = 'Article Sans Titre'): Response
+    public function show(string $slug): Response
     {
         return $this->render('blog/show.html.twig',
-            ['show' =>ucwords($slug,'-')]
+            ['show' =>ucwords(str_replace('-',' ',$slug))]
         );
     }
 }
